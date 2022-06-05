401a585dc67697b7928f478599ee123d45aa8029
Daniel <daniel.campos12@fatec.sp.gov.br>
2022-06-05 20:21:53 +0000

    Add schema get closest satellite by a given time and position

--- app/api/domain/schemas/schema_satellite.py
+++ app/api/domain/schemas/schema_satellite.py
@@ -19,3 +19,27 @@ class SatelliteResponse(BaseModel):
     creation_date: datetime
     latitude: float
     longitude: float
+
+
+class ClosestSatelliteRequest(BaseModel):
+    """This is a sample model to request the closest
+    Satellite distance by a given position on a globe
+    latitude, longitude coordinate and time"""
+
+    latitude: float = Field(0.0, description="The latitude coordinate. (-90 to 90)")
+    longitude: float = Field(0.0, description="The longitude coordinate. (-180 to 180")
+    given_time: Optional[datetime] = Field(
+                                        datetime.now(),
+                                        description="The given time for a last known position"
+                                    )
+
+
+class ClosestSatelliteResponse(BaseModel):
+    """This is a sample model for tracking
+    the closest satellite response"""
+    satellite_id: str
+    creation_date: datetime
+    latitude: float
+    longitude: float
+    distance: float
+    unit: str
